41c471f2e83c527222733b40b19e5211af0b343a
Konrad Marciniak <konrad.marciniak@microsoft.wsei.edu.pl>
2024-03-15 01:54:48 +0100

    Initial matrix class representation

---
+++ Matrix2D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix
+{
+    internal class Matrix2D
+    {
+        private readonly int[,] _Matrix;
+        public Matrix2D(int a, int b, int c, int d)
+        {
+            _Matrix = new int[2, 2];
+            _Matrix[0, 0] = a;
+            _Matrix[0, 1] = b;
+            _Matrix[1, 0] = c;
+            _Matrix[1, 1] = d;
+        }
+
+        public Matrix2D()
+        {
+            _Matrix = new int[2, 2];
+            _Matrix[0, 0] = 1;
+            _Matrix[0, 1] = 0;
+            _Matrix[1, 0] = 0;
+            _Matrix[1, 1] = 1;
+        }
+
+        public static readonly Matrix2D Id = new ();
+        public static readonly Matrix2D Zero = new (0, 0, 0, 0);
+
+        public override string ToString()
+        {
+            return $"[[{_Matrix[0, 0]}, {_Matrix[0, 1]}]\n [{_Matrix[1, 0]}, {_Matrix[1, 1]}]]";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix2D matrix = new Matrix2D(2, 2, 4, 4);
+            Matrix2D id = new Matrix2D();
+
+            Console.WriteLine(matrix);
+            Console.WriteLine(id);
+            Console.WriteLine(Matrix2D.Zero);
+        }
+    }
+}
