64902e33af4dc5220f0003ae520558d20f1f340e
Konrad Marciniak <konrad.marciniak@microsoft.wsei.edu.pl>
2024-03-15 12:28:20 +0100

    Refactoring - changed matrix property naming; Implemented IEquatable<T> interface

--- Matrix2D.cs
+++ Matrix2D.cs
@@ -6,33 +6,51 @@ using System.Threading.Tasks;
 
 namespace matrix
 {
-    internal class Matrix2D
+    internal class Matrix2D : IEquatable<Matrix2D>
     {
-        private readonly int[,] _Matrix;
+        private readonly int[,] _matrix;
         public Matrix2D(int a, int b, int c, int d)
         {
-            _Matrix = new int[2, 2];
-            _Matrix[0, 0] = a;
-            _Matrix[0, 1] = b;
-            _Matrix[1, 0] = c;
-            _Matrix[1, 1] = d;
+            _matrix = new int[2, 2];
+            _matrix[0, 0] = a;
+            _matrix[0, 1] = b;
+            _matrix[1, 0] = c;
+            _matrix[1, 1] = d;
         }
 
         public Matrix2D()
         {
-            _Matrix = new int[2, 2];
-            _Matrix[0, 0] = 1;
-            _Matrix[0, 1] = 0;
-            _Matrix[1, 0] = 0;
-            _Matrix[1, 1] = 1;
+            _matrix = new int[2, 2];
+            _matrix[0, 0] = 1;
+            _matrix[0, 1] = 0;
+            _matrix[1, 0] = 0;
+            _matrix[1, 1] = 1;
         }
 
-        public static readonly Matrix2D Id = new ();
-        public static readonly Matrix2D Zero = new (0, 0, 0, 0);
+        public static readonly Matrix2D Id = new();
+        public static readonly Matrix2D Zero = new(0, 0, 0, 0);
 
         public override string ToString()
         {
-            return $"[[{_Matrix[0, 0]}, {_Matrix[0, 1]}]\n [{_Matrix[1, 0]}, {_Matrix[1, 1]}]]";
+            return $"[[{_matrix[0, 0]}, {_matrix[0, 1]}]\n [{_matrix[1, 0]}, {_matrix[1, 1]}]]";
         }
+
+        public bool Equals(Matrix2D other)
+        {
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 2; j++)
+                    if (_matrix[i, j] != other._matrix[i, j])
+                        return false;
+            return true;
+        }
+
+        public static bool operator ==(Matrix2D? left, Matrix2D? right) =>
+            ReferenceEquals(left, right) || (left?.Equals(right) ?? false);
+
+        public static bool operator !=(Matrix2D left, Matrix2D right) =>
+            !(left == right);
+
+        public override int GetHashCode() =>
+            HashCode.Combine(_matrix[0, 0], _matrix[0, 1], _matrix[1, 0], _matrix[1, 1]);
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,14 +2,21 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Matrix2D matrix = new Matrix2D(2, 2, 4, 4);
             Matrix2D id = new Matrix2D();
 
             Console.WriteLine(matrix);
-            Console.WriteLine(id);
-            Console.WriteLine(Matrix2D.Zero);
+            Console.WriteLine(id.Equals(Matrix2D.Id));
+            Console.WriteLine(id == Matrix2D.Id);
+            Console.WriteLine(id == Matrix2D.Id);
+
+            Matrix2D matrix2 = new Matrix2D(2, 2, 4, 4);
+            Console.WriteLine(matrix != matrix2);
+
+            Console.WriteLine(id.GetHashCode());
+            Console.WriteLine(Matrix2D.Zero.GetHashCode());
         }
     }
 }
